e1ecf90b43dd8013781c191d4741026fa6d4ea33
wilfranr <wilfranr@gmail.com>
2022-06-04 17:27:09 -0500

    Título Cambiado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
             Seguimiento Pacientes {' '}
-            <span className="text-indigo-600">Veterinaria</span>
+            <span className="text-indigo-600">Veterinaria AnimaLocos!!</span>
         </h1>   
     )
 }
